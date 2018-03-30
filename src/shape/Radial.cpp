9f655de3240186981ed80bef7bb0f4eeb4bef193
liushiqi <liushiqi17@mails.ucas.ac.cn>
2018-03-30 12:37:51 +0800

    Add Radial.cpp

---
+++ src/shape/Radial.cpp
@@ -0,0 +1,12 @@
+//
+// Created by liu on 17-12-30.
+//
+
+#include "Radial.h"
+
+Radial::Radial(const Point &point, const Vector &direction) : point(point), direction(direction) {}
+
+Radial::Radial(const Point &point, double directionX, double directionY) : point(point),
+                                                                       direction(directionX, directionY) {}
+
+Radial::~Radial() = default;
